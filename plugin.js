a4aa5502691c0192d5193c2062cf6a007de8bda2
winstontom <93392068+winstontom@users.noreply.github.com>
2021-11-12 09:27:49 -0500

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,16 @@
+// NOW IN JAVASCRIPT MODE
+	var myMessage = "Hi Console!";
+	var getName = "name";
+	var getNumber = 0;
+	var gameNumber = 6;
+	var resultMsg = "Waiting to play";
+	
+	console.log(myMessage);
+	myMessage = "Guessing numbers between 1 and 5.";
+	document.write(myMessage);
+	
+// Function to play gameNumber
+	function play() {
+		
+	}
+// NOW OUT OF JAVASCRIPT MODE
