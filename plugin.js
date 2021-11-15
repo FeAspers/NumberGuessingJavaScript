c824e17090cd66018fdc21b2d207ec55c2d7c7c1
winstontom <93392068+winstontom@users.noreply.github.com>
2021-11-15 12:51:46 -0500

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -11,6 +11,23 @@
 	
 // Function to play gameNumber
 	function play() {
+		getName = prompt("Please enter your name :)", "name");
+		document.write("Hello, " + getName + "!<br><br>");
 		
-	}
+		gameNumber = Math.floor(Math.random() * 5) + 1;
+		console.log("Computer has chosen number: ", gameNumber);
+		document.write("Think of a number from 1 to 5.<br><br>");
+		
+		getNumber = prompt("Enter a number between 1 and 5.");
+		console.log(getName, "picked number: ", getNumber);
+		document.write("You picked number: " + String(getNumber) + " and Computer picked number: " + String(gameNumber));
+		
+		if(getNumber != gameNumber) {
+			resultMsg = " You guessed wrong!"
+		} else {
+			resultMsg = " WOW! You guessed the number!";
+		}
+		document.write(resultMsg)
+		}
+		
 // NOW OUT OF JAVASCRIPT MODE
